2b601461a491520560d50662e2333fa9c86bcf9b
P1ank <syedmohid314@gmail.com>
2022-12-05 23:33:37 -0500

    Production Ready

--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm , UserChangeForm ,PasswordChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm , EditProfileForm , PasswordChangingForm , ProfilePageForm
+from .forms import SignUpForm , EditProfileForm , PasswordChangingForm , ProfilePageForm 
 from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import DetailView , CreateView
 from ProBlog.models import Profile
@@ -37,7 +37,6 @@ class ShowProfilePageView(DetailView):
     def get_context_data(self, *args, **kwargs):
             users = Profile.objects.all()
             context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
-            
             page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
             
             context["page_user"] = page_user
@@ -46,7 +45,7 @@ class ShowProfilePageView(DetailView):
 class EditProfilePageView(generic.UpdateView):
     model = Profile
     template_name='registration/edit_profile_page.html'
-    fields=['bio','profile_pic']
+    form_class=ProfilePageForm
     success_url = reverse_lazy('home')
 
 class CreateProfilePageView(CreateView):
